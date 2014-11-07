c72082023bf06a1800c46d4415f6064ff22c585b
Wuvist <wuvist@gmail.com>
2014-11-07 17:27:09 +0800

    Update gothrift version

--- thrift/compact_protocol.go
+++ thrift/compact_protocol.go
@@ -31,6 +31,7 @@ const (
 	COMPACT_VERSION           = 1
 	COMPACT_VERSION_MASK      = 0x1f
 	COMPACT_TYPE_MASK         = 0x0E0
+	COMPACT_TYPE_BITS         = 0x07
 	COMPACT_TYPE_SHIFT_AMOUNT = 5
 )
 
@@ -335,7 +336,7 @@ func (p *TCompactProtocol) ReadMessageBegin() (name string, typeId TMessageType,
 	}
 	versionAndType, err := p.ReadByte()
 	version := versionAndType & COMPACT_VERSION_MASK
-	typeId = TMessageType((versionAndType >> COMPACT_TYPE_SHIFT_AMOUNT) & 0x03)
+	typeId = TMessageType((versionAndType >> COMPACT_TYPE_SHIFT_AMOUNT) & COMPACT_TYPE_BITS)
 	if err != nil {
 		return
 	}
@@ -421,11 +422,16 @@ func (p *TCompactProtocol) ReadFieldEnd() error { return nil }
 // "correct" types.
 func (p *TCompactProtocol) ReadMapBegin() (keyType TType, valueType TType, size int, err error) {
 	size32, e := p.readVarint32()
-	size = int(size32)
 	if e != nil {
 		err = NewTProtocolException(e)
 		return
 	}
+	if size32 < 0 {
+		err = invalidDataLength
+		return
+	}
+	size = int(size32)
+
 	keyAndValueType := byte(STOP)
 	if size != 0 {
 		keyAndValueType, err = p.ReadByte()
@@ -454,6 +460,10 @@ func (p *TCompactProtocol) ReadListBegin() (elemType TType, size int, err error)
 		size2, e := p.readVarint32()
 		if e != nil {
 			err = NewTProtocolException(e)
+			return
+		}
+		if size2 < 0 {
+			err = invalidDataLength
 			return
 		}
 		size = int(size2)
@@ -541,6 +551,10 @@ func (p *TCompactProtocol) ReadString() (value string, err error) {
 	if e != nil {
 		return "", NewTProtocolException(e)
 	}
+	if length < 0 {
+		return "", invalidDataLength
+	}
+
 	if length == 0 {
 		return "", nil
 	}
@@ -561,7 +575,10 @@ func (p *TCompactProtocol) ReadBinary() (value []byte, err error) {
 		return nil, NewTProtocolException(e)
 	}
 	if length == 0 {
-		return nil, nil //nil == empty slice
+		return []byte{}, nil
+	}
+	if length < 0 {
+		return nil, invalidDataLength
 	}
 
 	buf := make([]byte, length)
